d965754bd3d1a93712770dac2b033efa68aa1c47
sybe <54631194+sybetjeh@users.noreply.github.com>
2022-07-06 02:13:24 +0200

    Revert "Ran prettier + other changes to pass checks"
    This reverts commit b6bc7bc756a4ae5a4cfd8ed4566f4207680c26a1.

--- websites/R/Robloxforum/presence.ts
+++ websites/R/Robloxforum/presence.ts
@@ -1,7 +1,8 @@
 const presence = new Presence({
-		clientId: "993929577099448481",
-	}),
-	date = Date.now();
+	clientId: "993929577099448481",
+}),
+
+ date = Date.now();
 
 presence.on("UpdateData", async () => {
 	if (document.location.pathname === "/") {
@@ -16,9 +17,11 @@ presence.on("UpdateData", async () => {
 	} else if (document.location.pathname.startsWith("/members")) {
 		const presenceData: PresenceData = {
 			details: "Looking at a profile",
-			state: `Profile: ${document
-				.querySelector("meta[property='og:title']")
-				.getAttribute("content")}`,
+			state:
+				`Profile: ${ 
+				document
+					.querySelector("meta[property='og:title']")
+					.getAttribute("content")}`,
 			smallImageKey: "small",
 			smallImageText: "Profiles?!",
 			largeImageKey: "ig-mal",
@@ -40,9 +43,11 @@ presence.on("UpdateData", async () => {
 	} else if (document.location.pathname.startsWith("/thread")) {
 		const presenceData: PresenceData = {
 			details: "Viewing a thread",
-			state: `Thread: ${document
-				.querySelector("meta[property='og:title']")
-				.getAttribute("content")}`,
+			state:
+				`Thread: ${ 
+				document
+					.querySelector("meta[property='og:title']")
+					.getAttribute("content")}`,
 			smallImageKey: "small",
 			smallImageText: "Threading...",
 			largeImageKey: "ig-mal",
@@ -61,14 +66,17 @@ presence.on("UpdateData", async () => {
 	} else if (document.location.pathname.startsWith("/forums/")) {
 		const presenceData: PresenceData = {
 			details: "Viewing categories",
-			state: `Category: ${document
-				.querySelector("meta[property='og:title']")
-				.getAttribute("content")}`,
+			state:
+				`Category: ${ 
+				document
+					.querySelector("meta[property='og:title']")
+					.getAttribute("content")}`,
 			largeImageKey: "ig-mal",
 			startTimestamp: date,
 		};
 		presence.setActivity(presenceData);
 	} else {
+		
 		presence.setActivity({
 			details: "Vibing on RobloxForum",
 			largeImageKey: "ig-mal",
